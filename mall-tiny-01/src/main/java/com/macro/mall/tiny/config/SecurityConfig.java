bd55b004a35330f056ebe18a46b7a86af65d5826
张肯尼 <1097706841@qq.com>
2021-06-08 11:05:15 +0800

    修改swagger的配置 通过修改配置 实现调用接口自带Authorization头，这样就可以访问需要登陆的接口了

--- mall-tiny-01/src/main/java/com/macro/mall/tiny/config/SecurityConfig.java
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/config/SecurityConfig.java
@@ -3,10 +3,14 @@ package com.macro.mall.tiny.config;
 import com.macro.mall.tiny.component.JwtAuthenticationTokenFilter;
 import com.macro.mall.tiny.component.RestAuthenticationEntryPoint;
 import com.macro.mall.tiny.component.RestfulAccessDeniedHandler;
+import com.macro.mall.tiny.dto.AdminUserDetails;
+import com.macro.mall.tiny.mbg.model.UmsAdmin;
+import com.macro.mall.tiny.mbg.model.UmsPermission;
 import com.macro.mall.tiny.service.UmsAdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -14,9 +18,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
 
 /**
  * security的配置
@@ -39,14 +48,47 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     //用于配置需要拦截的url路径、jwt过滤器及出异常后的处理器
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()  //由于使用的是jwt，我们这里不需要 csrf
+                .disable()
+                .sessionManagement() //基于token，所以不需要session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, //允许对于网站静态资源的无授权访问
+                        "/",
+                        "/*.html",
+                        "favicon.ico",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js",
+                        "/swagger-resources/**",
+                        "/v2/api-docs/**"
+                )
+                .permitAll()
+                .antMatchers("/admin/login","/admin/register") //对于登录注册要允许匿名访问
+                .permitAll()
+                .antMatchers(HttpMethod.OPTIONS)//跨域请求会先进行一次option请求
+                .permitAll()
+                //.antMatchers("/**") //测试时全部允许访问
+                //.permitAll()
+                .anyRequest() //除了上面的所有请求全部需要及鉴权认证
+                .authenticated();
+        //禁用缓存
+        http.headers().cacheControl();
+        //添加jwt filter 过滤器
+        http.addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        //添加自定义未授权和未登录结果返回
+        http.exceptionHandling()
+                .accessDeniedHandler(restfulAccessDeniedHandler)
+                .authenticationEntryPoint(restAuthenticationEntryPoint);
     }
 
     //用于配置UserDetailsService及PasswordEncoder；
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder());
     }
 
     //SpringSecurity定义的用于对密码进行编码及比对的接口，目前使用的是BCryptPasswordEncoder；
@@ -58,15 +100,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     //SpringSecurity定义的核心接口，用于根据用户名获取用户信息，需要自行实现；
     @Bean
     public UserDetailsService userDetailsService(){
-
-        return null;
+        //获取登录用户信息
+        return username -> {
+            UmsAdmin admin = adminService.getAdminByUserName(username);
+            if(admin != null){
+                List<UmsPermission> permissionList = adminService.getPermissionList(admin.getId());
+                return new AdminUserDetails(admin,permissionList);
+            }
+            throw new UsernameNotFoundException("用户名或者密码错误");
+        };
     }
 
     //在用户名和密码校验前添加的过滤器，如果有jwt的token，会自行根据token信息进行登录。
     @Bean
     public JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter(){
-
-        return null;
+        return new JwtAuthenticationTokenFilter();
     }
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
